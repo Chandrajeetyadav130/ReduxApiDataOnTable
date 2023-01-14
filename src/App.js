5f322a01e7fcfe4e1cd80f344b5aca9158736f06
Chandrajeetyadav130 <chandrajeetyadav130@gmail.com>
2023-01-14 21:24:05 +0530

    Added mui project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Post from './Post';
+import Paper from '@mui/material/Paper';
 function App() {
   return (
     <>
-            <div className=" card container-fluid container-sm container-md" style={{border:"1px light black" ,marginTop:"20px",marginBottom:"20px"}}>
+            <Paper elevation={4} variant="outlined" sx={{margin:"1rem"}} >
 
     <h5 style={{textAlign:"center",marginTop:"1rem"}}>Display all Data of Api using Redux</h5>
    <Post/>
-   </div>
+   </Paper>
     </>
   );
 }
